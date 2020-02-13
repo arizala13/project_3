40206515985f56ff93a3fcce04fa063e4f6acf0c
Andres Arizala <arizala13@gmail.com>
2020-02-13 13:22:52 -0600

    Making links on business card work - need to get email to work

--- lib/main.dart
+++ lib/main.dart
@@ -28,18 +28,28 @@ class MyApp extends StatelessWidget {
                   AspectRatio(
                     aspectRatio: 4 /2,
                     child: Image.asset('assets/Andres-Arizala.jpg')),
-                  /* Column(
+                   Column(
                     children: <Widget>[
-                      Center(child: Text('Andres Arizala \n', textAlign: TextAlign.center)),
-                      Center(child: Text('832-215-9876 \n', textAlign: TextAlign.center)),
-                      Center(child: Text('Andres Arizala \n', textAlign: TextAlign.center)),
-                      Center(child: GestureDetector(onTap: () => launch('sms:5550101234'))),
-                    ], 
-                  ), */ 
-                  Center(child: GestureDetector(
-                    onTap: () => launch('sms:5550101234'),
-                    child: Text('Andres Arizala \n', textAlign: TextAlign.center))
+                  Center(
+                    child: Text('Andres Arizala \n', textAlign: TextAlign.center)
                     ),
+                                      Center(
+                    child: Text('Software Engineer \n', textAlign: TextAlign.center)
+                    ),
+                                      Center(child: GestureDetector(
+                    onTap: () => launch('sms:8322159876'),
+                    child: Text('832-215-9876 \n', textAlign: TextAlign.center))
+                    ),
+                                      Center(child: GestureDetector(
+                    onTap: () => launch('https:github.com/arizala13'),
+                    child: Text('github.com/arizala13 \n', textAlign: TextAlign.center))
+                    ),
+                                                          Center(child: GestureDetector(
+                    onTap: () => launch('mailto:arizala13@gmail.com?subject=News&body=New%20plugin'),
+                    child: Text('arizala13@gmail.com \n', textAlign: TextAlign.center))
+                    ),
+                    ], 
+                  ),
                 ],
               ),
               Text('Resume', textAlign: TextAlign.center,),
